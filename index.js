56ee1854a460132718abb30cef4303cc7a46da67
oualidelhouari <elhouarioualid@gmail.com>
2023-08-06 18:30:54 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+    return bestCustomer = 'not bob';
+}
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'bob';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "BOB"
+}
